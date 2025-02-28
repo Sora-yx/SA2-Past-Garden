4fa736eba390a7060c5fdf1b10cbd3ae8aab52ef
Sora <sora.x@hotmail.fr>
2025-02-28 12:19:38 +0100

    Removing that

--- SA2-Past-Garden/past-garden.cpp
+++ SA2-Past-Garden/past-garden.cpp
@@ -286,11 +286,9 @@ void init_PastGarden_Level()
 
 	init_ChaoFixes_Hack();
 
-	//issues is below
-	// 
-	// 
+
 	//prevent the game to load a ton of chao stuff we will call them after.
-	//WriteData<5>((int*)0x54C9C2, 0x90);  //spawn chao create	
+
 	WriteData<5>((int*)0x54C9B8, 0x90);	//MinimalCreate
 	WriteData<5>((int*)0x54C9C7, 0x90);  //Chao Tree
 	WriteData<5>((int*)0x54CA95, 0x90); //ball toy (col despawn if too far)
